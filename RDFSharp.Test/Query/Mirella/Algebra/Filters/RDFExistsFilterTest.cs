1d315dc1c0d86e63416d17f04ddc17ac7343abdf
PinterVivien <79516856+PinterVivien@users.noreply.github.com>
2022-05-15 09:45:16 +0200

    RDFExistsFilter unit tesztek befejezés.

--- RDFSharp.Test/Query/Mirella/Algebra/Filters/RDFExistsFilterTest.cs
+++ RDFSharp.Test/Query/Mirella/Algebra/Filters/RDFExistsFilterTest.cs
@@ -10,8 +10,9 @@ namespace RDFSharp.Test.Query.Mirella.Algebra.Filters
     [TestClass]
     public class RDFExistsFilterTest
     {
+        #region RDFExistsFilter
         [TestMethod]
-        public void ShouldThrowExceptionOnRDFExistsFilterBecauseLeftFilterNull()
+        public void ShouldThrowExceptionOnRDFExistsFilterBecauseFilterNull()
              => Assert.ThrowsException<RDFQueryException>(() => new RDFExistsFilter(null));
 
         [TestMethod]
@@ -112,5 +113,98 @@ namespace RDFSharp.Test.Query.Mirella.Algebra.Filters
             Assert.IsTrue(n.ApplyFilter(row2, false));
             Assert.IsFalse(n.ApplyFilter(row2, true));
         }
+        #endregion
+
+        #region RDFNotExistsFilter
+       
+        [TestMethod]
+        public void RDFNotExistsFilterOK()
+        {
+            RDFVariable x = new RDFVariable("x");
+            RDFVariable y = new RDFVariable("y");
+            RDFResource knows = new RDFResource(RDFVocabulary.DC.BASE_URI + "knows");
+            var pattern = new RDFPattern(x, knows, y);
+            RDFNotExistsFilter filter = new RDFNotExistsFilter(pattern);
+            Assert.AreEqual<RDFPattern>(pattern, filter.Pattern);
+            Assert.IsTrue(filter.IsEvaluable);
+        }
+
+        [TestMethod]
+        public void RDFNotExistsFilterToString()
+        {
+            //Arrange
+            RDFVariable x = new RDFVariable("x");
+            RDFVariable y = new RDFVariable("y");
+            RDFResource knows = new RDFResource(RDFVocabulary.DC.BASE_URI + "knows");
+            var pattern = new RDFPattern(x, knows, y);
+
+            //Act
+            RDFNotExistsFilter n = new RDFNotExistsFilter(pattern);
+            string msg = n.ToString();
+
+            //Assert
+            Assert.AreEqual<string>("FILTER ( NOT EXISTS { ?X <http://purl.org/dc/elements/1.1/knows> ?Y } )", msg);
+        }
+
+        [TestMethod]
+        public void RDFNotExistsFilterApplyFilterContains()
+        {
+            //Arrange
+            RDFVariable x = new RDFVariable("x");
+            RDFVariable y = new RDFVariable("y");
+            RDFResource knows = new RDFResource(RDFVocabulary.DC.BASE_URI + "knows");
+            var pattern = new RDFPattern(x, knows, y);
+            RDFExistsFilter n = new RDFExistsFilter(pattern);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = null;
+            row["?B"] = new RDFPlainLiteral("hello", "en-US").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            n.PatternResults = table;
+
+            //Assert
+            Assert.IsTrue(n.ApplyFilter(row, false));
+        }
+
+        [TestMethod]
+        public void RDFNotExistsFilterApplyFilterNotContains()
+        {
+            //Arrange
+            RDFVariable x = new RDFVariable("x");
+            RDFVariable y = new RDFVariable("y");
+            RDFResource knows = new RDFResource(RDFVocabulary.DC.BASE_URI + "knows");
+            var pattern = new RDFPattern(x, knows, y);
+            RDFNotExistsFilter n = new RDFNotExistsFilter(pattern);
+
+            DataTable table = new DataTable();
+            table.Columns.Add("?A", typeof(string));
+            table.Columns.Add("?B", typeof(string));
+            DataRow row = table.NewRow();
+            row["?A"] = null;
+            row["?B"] = new RDFPlainLiteral("hello", "en-US").ToString();
+            table.Rows.Add(row);
+            table.AcceptChanges();
+
+            n.PatternResults = table;
+
+            DataTable table2 = new DataTable();
+            table.Columns.Add("?C", typeof(string));
+            table.Columns.Add("?D", typeof(string));
+            DataRow row2 = table.NewRow();
+            row2["?C"] = null;
+            row2["?D"] = new RDFPlainLiteral("bonjour", "fr-FR").ToString();
+            table.Rows.Add(row2);
+            table.AcceptChanges();
+
+            //Assert
+            Assert.IsTrue(n.ApplyFilter(row2, true));
+            Assert.IsFalse(n.ApplyFilter(row2, false));
+        }
+        #endregion
     }
 }
