f93e54e66658b512a005dae6f59bdb4815add7da
maya2d26 <78872766+maya2d26@users.noreply.github.com>
2022-05-17 22:05:21 +0200

    Revert "Disabled acces to external entities in Xml parsing"

--- RDFSharp/Model/Serializers/RDFXml.cs
+++ RDFSharp/Model/Serializers/RDFXml.cs
@@ -379,7 +379,6 @@ namespace RDFSharp.Model
                 {
                     using (XmlTextReader xmlReader = new XmlTextReader(streamReader))
                     {
-                        xmlReader.XmlResolver = null;
                         xmlReader.DtdProcessing = DtdProcessing.Parse;
                         xmlReader.Normalization = false;
 
--- RDFSharp/Query/Mirella/Algebra/Queries/RDFSelectQueryResult.cs
+++ RDFSharp/Query/Mirella/Algebra/Queries/RDFSelectQueryResult.cs
@@ -231,7 +231,6 @@ namespace RDFSharp.Query
                 {
                     using (XmlTextReader xmlReader = new XmlTextReader(streamReader))
                     {
-                        xmlReader.XmlResolver = null;
                         xmlReader.DtdProcessing = DtdProcessing.Parse;
                         xmlReader.Normalization = false;
 
