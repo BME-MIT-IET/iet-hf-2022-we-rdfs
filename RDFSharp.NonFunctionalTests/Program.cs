4aaf548e1bdec3ae54f7d83efe61b3e6a6bafe0d
maya2d26 <maya2d26@gmail.com>
2022-05-17 23:20:55 +0200

    Creating a project for testing

---
+++ RDFSharp.NonFunctionalTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using RDFSharp.Model;
+using RDFSharp.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDFSharp.ManualTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
